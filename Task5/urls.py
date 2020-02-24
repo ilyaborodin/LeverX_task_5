259c3cbfb9bf254cf57a3a3b594dfca994c3f505
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-24 21:35:06 +0300

    Changed url for api

--- Task5/urls.py
+++ Task5/urls.py
@@ -7,5 +7,5 @@ schema_view = get_swagger_view(title='Pastebin API')
 urlpatterns = [
     path('swagger/', schema_view),
     path('admin/', admin.site.urls),
-    path('api/v1/courses/', include('courses.urls')),
+    path('api/v1/', include('courses.urls')),
 ]
