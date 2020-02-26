39c4df070872bc2a23faa8a1253879448f2202c7
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-26 11:13:27 +0300

    Deleted teacher and student from admin.py

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import Student, User, Teacher
+from .models import User
 
 admin.site.register(User)
-admin.site.register(Student)
-admin.site.register(Teacher)
