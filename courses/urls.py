df1917e1b7773dbd818c7cdc3740db214b7c8995
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-26 22:21:32 +0300

    Reformatted file urls.py

--- courses/urls.py
+++ courses/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
+from .views import CourseCreateView, CoursesListView
 
+# auth
 urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+]
+# courses
+urlpatterns += [
+    path('teacher/courses/course/create', CourseCreateView.as_view()),
+    path('student/courses/all/', CoursesListView.as_view()),
 ]
