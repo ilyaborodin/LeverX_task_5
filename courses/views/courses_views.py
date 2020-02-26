4fd6bb433439fd8e5c639b4ee447c4e4ff4a64fe
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-26 23:44:13 +0300

    Split file views to multiply files

---
+++ courses/views/courses_views.py
@@ -0,0 +1,75 @@
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import generics
+from Task5.courses.serializers import CourseDetailSerializer, CoursesListSerializer
+from Task5.courses.models import Course
+from Task5.courses.permissions import IsStudent, IsTeacher, IsOwnerOrReadOnly
+from django.db.models import Q
+
+
+@permission_classes((IsAuthenticated, IsTeacher))
+class CourseCreateView(generics.CreateAPIView):
+    """
+    Create Course
+    Available for teachers
+    """
+    serializer_class = CourseDetailSerializer
+
+
+@permission_classes((IsAuthenticated, IsOwnerOrReadOnly))
+class CoursesDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update, destroy course
+    Available for all users
+    """
+    serializer_class = CourseDetailSerializer
+    queryset = Course.objects.all()
+
+
+@permission_classes((IsAuthenticated, IsStudent))
+class CoursesListView(generics.ListAPIView):
+    """
+    Return all courses
+    Available for students
+    """
+    serializer_class = CoursesListSerializer
+    queryset = Course.objects.all()
+
+
+@permission_classes((IsAuthenticated, IsTeacher))
+class AuthorsCoursesListView(generics.ListAPIView):
+    """
+    Return list of courses where user is creator
+    Available for Teachers
+    """
+    serializer_class = CoursesListSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Course.objects.filter(author=user)
+
+
+@permission_classes((IsAuthenticated, IsTeacher))
+class TeachersCoursesListView(generics.ListAPIView):
+    """
+    Return list of courses where user is teacher and isn't creator
+    Available for Teachers
+    """
+    serializer_class = CoursesListSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Course.objects.filter(teachers=user)
+
+
+@permission_classes((IsAuthenticated, IsTeacher))
+class TeachersAndAuthorsCoursesListView(generics.ListAPIView):
+    """
+    Return list of courses where user can be teacher or creator
+    Available for Teachers
+    """
+    serializer_class = CoursesListSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Course.objects.filter(Q(teachers=user) | Q(author=user))
