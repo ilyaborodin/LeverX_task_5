7fe52288a3d245c39b53d959ea69a32a332b1ab9
Ilya Borodin <ilya.borodin8@gmail.com>
2020-02-27 00:09:05 +0300

    Added all urls for views

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import CourseCreateView, CoursesListView
+from .views import courses_views
 
 # auth
 urlpatterns = [
@@ -8,6 +8,10 @@ urlpatterns = [
 ]
 # courses
 urlpatterns += [
-    path('teacher/courses/course/create', CourseCreateView.as_view()),
-    path('student/courses/all/', CoursesListView.as_view()),
+    path('courses/teachers/course/create', courses_views.CourseCreateView.as_view()),
+    path('courses/teachers/all/', courses_views.TeachersAndAuthorsCoursesListView.as_view()),
+    path('courses/teachers/author/', courses_views.AuthorsCoursesListView.as_view()),
+    path('courses/teachers/teacher/', courses_views.TeachersCoursesListView.as_view()),
+    path('courses/student/all/', courses_views.CoursesListView.as_view()),
+    path('courses/course/detail/<int:pk>/', courses_views.CourseDetailSerializer)
 ]
