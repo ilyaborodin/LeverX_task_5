05819621a9d9a70c09bd4ddb08c91717589d5232
Ilya Borodin <ilya.borodin8@gmail.com>
2020-03-01 17:27:00 +0300

    Changed token lifetime

--- Task5/settings.py
+++ Task5/settings.py
@@ -139,8 +139,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
-   'REFRESH_TOKEN_LIFETIME': timedelta(days=10),
+   'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
+   'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
 AUTH_USER_MODEL = 'courses.User'
